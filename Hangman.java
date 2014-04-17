6011db77acc386483268530de3bf18662c2fba3d
S <s>
2014-04-16 22:58:47 -0400

    {"type":"TI", "runs":"0"}

--- Hangman.java
+++ Hangman.java
@@ -15,6 +15,15 @@ public class Hangman extends ConsoleProgram {
 
     public void run() {
 		/* You fill this in */
+    	println("Welcome to Hangman!");
+    	
+    	/* Randomly choose a word from Hangman Lexicon */
+    	
+    	int index = rgen.nextInt(0, HangmanLexicon.getWordCount());
+    	
 	}
 
+    
+    /* Instant variables */
+    private RandomGenerator rgen = RandomGenerator.getInstance();
 }
