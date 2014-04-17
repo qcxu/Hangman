18798355b80bd3865716eb95758f109dc6d4fb4c
S <s>
2014-04-17 17:27:34 -0400

    {"type":"TI", "runs":"0"}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -38,6 +38,8 @@ public class HangmanCanvas extends GCanvas {
  * been guessed so far; unguessed letters are indicated by hyphens.
  */
 	public void displayWord(String word) {
+		if ()
+		remove(wordLabel);
 		wordLabel = new GLabel(word, 0.1*getWidth(), 0.9*getHeight());
 		wordLabel.setFont("Purisa-26");
 		add(wordLabel);
