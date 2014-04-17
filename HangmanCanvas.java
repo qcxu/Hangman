45af5ed01f445a7f05d4c52be12ecb92ab3cee56
S <s>
2014-04-17 17:00:58 -0400

    {"type":"TI", "runs":"0"}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -63,5 +63,6 @@ public class HangmanCanvas extends GCanvas {
 	private static final int HIP_WIDTH = 36;
 	private static final int LEG_LENGTH = 108;
 	private static final int FOOT_LENGTH = 28;
+	String inco_update = "";
 
 }
